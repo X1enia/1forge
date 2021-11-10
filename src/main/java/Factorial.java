22aeadc1ee8a14133b7030a848217e7bb387c54e
X1enia <xrvikhoreva@gmail.com>
2021-11-11 00:24:06 +0300

    Factorial Вычисление факториала числа n!

---
+++ src/main/java/Factorial.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Factorial {
+
+    public static void main(String[] args) {
+        Scanner sn = new Scanner(System.in);
+        System.out.println("Задайте число: ");
+        int n = sn.nextInt();
+        int result = 1;
+
+        for (int i = 1; i <= n; i++) {
+            result = result * i;
+        }
+
+        System.out.println(result);
+
+    }
+
+}
