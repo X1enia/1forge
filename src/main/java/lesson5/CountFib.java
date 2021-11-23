59cf72e0ff2eb93a532714c7f63f55b248b48bd0
X1enia <xrvikhoreva@gmail.com>
2021-11-23 22:02:01 +0300

    Исправлена работа программы.

--- src/main/java/lesson5/CountFib.java
+++ src/main/java/lesson5/CountFib.java
@@ -1,24 +1,28 @@
 package lesson5;
 
+import lesson6.Fibonacci;
+
 import java.util.Scanner;
 
 public class CountFib {
 
     public static void main(String[] args) {
         Scanner sn = new Scanner(System.in);
+        while (true) {
+            System.out.println("Введите число: ");
+            int countFib = sn.nextInt();
 
-        System.out.println("Введите число: ");
-        int countFib = sn.nextInt();
-
-        int fibFirst = 1;
-        int fibSecond = 1;
-        int fibNext = 0;
-
-        for (int i = 2; i < countFib; i++) {
-            fibNext = fibFirst + fibSecond;
-            fibFirst = fibSecond;
-            fibSecond = fibNext;
+            int[] fibArray = new int[countFib];
+            fibArray[0] = 0;
+            if (fibArray.length > 1) {
+                fibArray[1] = 1;
+            }
+            if (fibArray.length > 1) {
+                for (int i = 2; i < fibArray.length; i++) {
+                    fibArray[i] = fibArray[i - 1] + fibArray[i - 2];
+                }
+            }
+            System.out.println(fibArray[countFib - 1]);
         }
-        System.out.println("" + fibNext);
     }
 }
