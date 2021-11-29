4ab6f5ae2362663a587b018eb8caaf9c89c89b25
X1enia <xrvikhoreva@gmail.com>
2021-11-29 23:33:09 +0300

    Вычисление ряда Фибоначчи, используя массив как промежуточный кэш.

--- src/main/java/lesson6/Fibonacci.java
+++ src/main/java/lesson6/Fibonacci.java
@@ -7,10 +7,15 @@ public class Fibonacci {
     public static void main(String[] args) {
         Scanner sn = new Scanner(System.in);
 
-        long[] fibArray = calculateFibCache(10);
+        int cashSize = 50;
+        long[] fibArray = new long[cashSize];
         boolean isNotCorrect = true;
         int num = 0;
 
+        fibArray[0] = 1;
+        fibArray[1] = 1;
+        int fibIndex = 2;
+        calculateFibCache(5, fibIndex, fibArray);
         while (isNotCorrect) {
             System.out.println("Введите число: ");
             if (sn.hasNextInt()) {
@@ -18,26 +23,26 @@ public class Fibonacci {
                 System.out.println("Я получил число " + num);
                 if (num < 1) {
                     System.out.println("Введите число больше или равное 1!");
+                } if (num >= cashSize) {
+                    System.out.println("Введите число меньше 50!");
                 } else {
                     System.out.println("Спасибо за целое число!");
                     isNotCorrect = false;
+                    if (num >= fibIndex) {
+                        calculateFibCache(num + 1, fibIndex, fibArray);
+                        System.out.println(fibArray[num]);
+                        fibIndex = num;
+                    } else {
+                        System.out.println(fibArray[num]);
+                    }
                 }
             }
-            if (num >= fibArray.length) {
-                fibArray = calculateFibCache(num + 1);
-            }
-            System.out.println(fibArray[num]);
         }
     }
 
-    public static long[] calculateFibCache(int numFib) {
-        long[] fibArray = new long[numFib];
-        fibArray[0] = 0;
-        fibArray[1] = 1;
-
-        for (int i = 2; i < fibArray.length; i++) {
-            fibArray[i] = fibArray[i - 1] + fibArray[i - 2];
+    public static void calculateFibCache(int numFib, int fibIndex, long[] fibCache) {
+        for (int i = fibIndex; i < numFib; i++) {
+            fibCache[i] = fibCache[i - 1] + fibCache[i - 2];
         }
-        return fibArray;
     }
 }
