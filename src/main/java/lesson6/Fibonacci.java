0b74fad562ae61c3c1ec700fe1159207f8b59dad
X1enia <xrvikhoreva@gmail.com>
2021-12-05 02:03:40 +0300

    Правки в методе по вычислению чисел Фибоначчи

--- src/main/java/lesson6/Fibonacci.java
+++ src/main/java/lesson6/Fibonacci.java
@@ -16,6 +16,7 @@ public class Fibonacci {
         fibArray[1] = 1;
         int fibIndex = 2;
         calculateFibCache(5, fibIndex, fibArray);
+        fibIndex = 4;
         while (isNotCorrect) {
             System.out.println("Введите число: ");
             if (sn.hasNextInt()) {
@@ -23,6 +24,7 @@ public class Fibonacci {
                 System.out.println("Я получил число " + num);
                 if (num < 1) {
                     System.out.println("Введите число больше или равное 1!");
+                    num = sn.nextInt();
                 } if (num >= cashSize) {
                     System.out.println("Введите число меньше 50!");
                 } else {
@@ -30,10 +32,10 @@ public class Fibonacci {
                     isNotCorrect = false;
                     if (num >= fibIndex) {
                         calculateFibCache(num + 1, fibIndex, fibArray);
-                        System.out.println(fibArray[num]);
-                        fibIndex = num;
+                        System.out.println(fibArray[num - 1]);
+                        fibIndex = num - 1;
                     } else {
-                        System.out.println(fibArray[num]);
+                        System.out.println(fibArray[num - 1]);
                     }
                 }
             }
